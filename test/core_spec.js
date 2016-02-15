632f64ca668bb3fbca08b8c2282579a9b563db96
Kevin Lanzon <kevinlanzon@gmail.com>
2016-02-15 14:26:13 +0000

    merges an update into the old state

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries} from '../js/core';
+import {setEntries, next} from '../js/core';
 
 describe('app logic', function() {
 
@@ -12,6 +12,31 @@ describe('app logic', function() {
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('A New Hope', 'The Empire Strikes Back')
+      }));
+    });
+
+    it('converts to immutable', function() {
+      const state = Map();
+      const entries = ['A New Hope', 'The Empire Strikes Back'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('A New Hope', 'The Empire Strikes Back')
+      }));
+    });
+  });
+
+  describe('next', function() {
+
+    it('takes the next two entries under vote', function() {
+      const state = Map({
+        entries: List.of('A New Hope', 'The Empire Strikes Back', 'Return of The Jedi')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('A New Hope', 'The Empire Strikes Back')
+        }),
+        entries: List.of('Return of The Jedi')
       }));
     });
   });
