b8024b5925211120407e9e5188caf498152e8a96
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-23 15:04:28 +0000

    adds gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      js: {
+        files: ['js/js/*.js'],
+        tasks: ['uglify:dev']
+      }
+    },
+    es6transpiler: {
+      dist: {
+        files: {
+          'dist/app.js': 'src/app.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['es6transpiler']);
+
+
+};
